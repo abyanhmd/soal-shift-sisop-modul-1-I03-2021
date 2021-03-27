7aec2b0173e0743995a6406decae9b19b8553387
abyanhmd <74693948+abyanhmd@users.noreply.github.com>
2021-03-27 12:48:13 +0700

    Update soal1.sh

--- soal1.sh
+++ soal1.sh
@@ -11,4 +11,8 @@ echo $logerror >> pertama.txt
 sumloginfo="$(grep -E 'ERROR' /var/log/syslog | wc -l)"
 echo $sumloginfo >> pertama.txt"
 
-# 1C
+# 3A
+for i in {1..23}
+do
+        wget -O Koleksi_$i https://loremflickr.com/320/240/kitten
+done
