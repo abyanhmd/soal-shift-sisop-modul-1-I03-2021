d03e329f795596c74bfe193b4c3e3eb3660a82e4
Mukhoram <74299958+Mukhoram@users.noreply.github.com>
2021-04-03 11:29:35 +0700

    Update 2a.sh

--- 2a.sh
+++ 2a.sh
@@ -40,3 +40,26 @@ BEGIN{printf "customer list of albuquerque at 2017\n"}
 END{ for(b in a){ print b} {printf "\n"}}
 ' "$input" >> hasil.txt
 
+#2c
+#!/bin/bash
+export LC_ALL=C
+input="/home/mukhoram/Downloads/Laporan-TokoShiSop.tsv"
+awk -F"\t"  '
+BEGIN {printf("the least segment type is")}
+{segment=$8
+    if (NR == 1) next 
+    count[segment]++
+} 
+END {type=0;min=99999;
+for (segment in count) {
+    if (min > count[segment]) {
+        min=count[segment]
+        type=segment
+    }
+}
+    printf(" %s with %s\n" , type, min)
+}' "$input"  >> hasil.txt
+
+#2d
+
+
