58d434b8dcc58a128c04479b9a069a8a03e85ebb
Mukhoram <74299958+Mukhoram@users.noreply.github.com>
2021-04-03 10:52:52 +0700

    Update soal2_generate_laporan_ihir_shisop_2a.sh

--- soal2_generate_laporan_ihir_shisop_2a.sh
+++ soal2_generate_laporan_ihir_shisop_2a.sh
@@ -19,3 +19,21 @@ BEGIN{printf "transaction with the biggest profit precentage" }
 }
 END{printf " %d with precentage of %.2f%%.\n\n", idmax, max}
 ' "$input" >> hasil.txt
+//////
+#!/bin/bash
+export LC_ALL=C
+input="/home/mukhoram/Downloads/Laporan-TokoShiSop.tsv"
+awk -F "\t" '
+BEGIN{printf "customer list of albuquerque at 2017\n"}
+{
+	{if(NR!=1)
+		{
+			{if($10~"Albuquerque" && $3 ~ /17$/)
+					a[$7]++
+			}
+		}
+	}
+}
+END{ for(b in a){ print b} {printf "\n"}}
+' "$input" >> hasil.txt
+////////
