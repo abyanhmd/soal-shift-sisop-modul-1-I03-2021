663d2f8ea2d42b28d1b2f214fe4c55bf5565b4cd
HilmyThoriq <81411468+HilmyThoriq@users.noreply.github.com>
2021-04-03 11:21:14 +0800

    Update soal3a.sh (revised)

--- soal3a.sh
+++ soal3a.sh
@@ -1,6 +1,19 @@
 #!/bin/bash
 
-for i in {1..23}
+# * Downloads file
+for (( i=0 ; i<23 ; i=i+1 ))
 do
-wget -O Koleksi_$i https://loremflickr.com/320/240/kitten -o foto.log
+wget --content-disposition https://loremflickr.com/320/240/kitten -a 'Foto.log'
 done
+
+# * remove duplicates with jpg
+rm *.jpg.* 
+
+# # * Rename .jpg incrementing
+j=1
+for i in *.jpg
+do
+mv "$i" Koleksi_"$j".jpg
+let j=j+1
+done
+
