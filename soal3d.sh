23beb7bc5bac4e18818a9a10938915db5bac3852
HilmyThoriq <81411468+HilmyThoriq@users.noreply.github.com>
2021-04-03 12:11:47 +0800

    Add files via upload

---
+++ soal3d.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+
+jam=`date +%H`
+
+if [[ $jam == 7 ]]
+then
+    zip -r -P $(date +%m%d%Y) Koleksi.zip */
+    # remove all zip
+    rm -r */
+elif [[ $jam == 18 ]]
+then
+    unzip -P $(date +%m%d%Y) Koleksi.zip
+    rm Koleksi.zip
+fi
